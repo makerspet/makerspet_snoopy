e0ab1a7caba9dbbdd8ee1a43daed062d5942a72c
Ilia O. <iliao@makerspet.com>
2023-11-22 20:05:38 -0800

    Copyright

--- arduino/snoopy-esp32/snoopy-esp32.ino
+++ arduino/snoopy-esp32/snoopy-esp32.ino
@@ -1,9 +1,16 @@
-// Scan publishes, WiFi AP works
-
-//TODO detect micro-ros agent disconnect
-//TODO debug /odom NaN
-//TODO discover ROS2 PC automatically
-//  #define RMW_UXRCE_TRANSPORT_UDP
+// Copyright 2023 REMAKE AI
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "robot_config.h"
 #include "util.h"
@@ -12,8 +19,6 @@
 #include <micro_ros_kaia.h>
 #include <HardwareSerial.h>
 #include "YDLidar.h"
-//#include <sys/time.h>
-//#include "time.h"
 #include <rcl/rcl.h>
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
